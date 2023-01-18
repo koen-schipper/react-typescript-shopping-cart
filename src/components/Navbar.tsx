e984ea62149301b6bd34e6280cf7363cdc30c3cb
Koen Schipper <koen.schipper@me.com>
2023-01-18 19:14:58 +0100

    final shopping cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export function Navbar() {
+    const { openCart, cartQuantity } = useShoppingCart();
+
     return (
         <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
             <Container>
@@ -16,37 +19,41 @@ export function Navbar() {
                         About
                     </Nav.Link>
                 </Nav>
-                <Button
-                    style={{ position: 'relative' }}
-                    variant='outline-success'
-                    className='rounded-square'
-                >
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='22'
-                        height='22'
-                        fill='currentColor'
-                        className='bi bi-cart'
-                        viewBox='0 0 16 16'
+
+                {cartQuantity > 0 && (
+                    <Button
+                        onClick={openCart}
+                        style={{ position: 'relative' }}
+                        variant='outline-success'
+                        className='rounded-square'
                     >
-                        {' '}
-                        <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />{' '}
-                    </svg>
-                    <div
-                        className='rounded-circle bg-danger text-white d-flex justify-content-center align-items-center'
-                        style={{
-                            fontSize: '0.9rem',
-                            width: '1.2rem',
-                            height: '1.2rem ',
-                            position: 'absolute',
-                            bottom: 0,
-                            right: 0,
-                            transform: 'translate(35%, 35%)'
-                        }}
-                    >
-                        3
-                    </div>
-                </Button>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='22'
+                            height='22'
+                            fill='currentColor'
+                            className='bi bi-cart'
+                            viewBox='0 0 16 16'
+                        >
+                            {' '}
+                            <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />{' '}
+                        </svg>
+                        <div
+                            className='rounded-circle bg-danger text-white d-flex justify-content-center align-items-center'
+                            style={{
+                                fontSize: '0.9rem',
+                                width: '1.2rem',
+                                height: '1.2rem ',
+                                position: 'absolute',
+                                bottom: 0,
+                                right: 0,
+                                transform: 'translate(35%, 35%)'
+                            }}
+                        >
+                            {cartQuantity}
+                        </div>
+                    </Button>
+                )}
             </Container>
         </NavbarBs>
     );
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
+import { ShoppingCart } from '../components/ShoppingCart';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 type ShoppingCartProviderProps = {
     children: ReactNode;
@@ -27,9 +29,14 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>('shopping-cart', []);
 
     const cartQuantity = cartItems.reduce((quantity, item) => quantity + item.quantity, 0);
+
+    const openCart = () => setIsOpen(true);
+
+    const closeCart = () => setIsOpen(false);
 
     function getItemQuantity(id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0;
@@ -75,11 +82,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
                 increaseCartQuantity,
                 decreaseCartQuantity,
                 removeFromCart,
+                openCart,
+                closeCart,
                 cartItems,
                 cartQuantity
             }}
         >
             {children}
+            <ShoppingCart isOpen={isOpen} />
         </ShoppingCartContext.Provider>
     );
 }
